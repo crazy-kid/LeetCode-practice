e8798d02c9c35cfa45c9049592444f317d082234
tomorrow_morning_will_be_fine <1005489594@qq.com>
2023-05-30 15:33:37 +0800

    2023-5-30

--- JAVA/Mid/mid_46.java
+++ JAVA/Mid/mid_46.java
@@ -1,6 +1,27 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
 public class mid_46 {
 
     public static void main(String[] args) {
+        int[] nums = { 1, 2, 3 };
 
+        List<List<Integer>> list = new ArrayList<List<Integer>>();
+        int n = nums.length;
+
+    }
+
+    public static void brackTrack(List<List<Integer>> list, int first, int[] nums) {
+        if (first == nums.length)
+            list.add(Arrays.stream(nums).boxed().collect(Collectors.toList()));
+    }
+
+    public static void swap(int i, int j, int[] nums) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
     }
 }
--- JAVA/test.java
+++ JAVA/test.java
@@ -1,17 +1,19 @@
-package JAVA;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Stack;
+import java.util.stream.Collectors;
 
 public class test {
     public static int t = 1702766719;
 
     public static void main(String[] args) {
-
-        StringBuilder s = new StringBuilder("null");
-        System.out.println(s.toString());
+        int[] nums = { 1, 2, 3, 4 };
+        List<Integer> list = new ArrayList<>();
+        list = Arrays.stream(nums).boxed().collect(Collectors.toList());
+        for (int i : list)
+            System.out.println(i);
     }
 }
